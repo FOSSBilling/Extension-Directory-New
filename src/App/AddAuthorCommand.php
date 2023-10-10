9db36f6bbdce5d1de7916c32281bd37dcdcd671c
Belle Aerni <belleaerni@gmail.com>
2023-10-10 09:49:44 -0700

    Improved the add-author command

--- src/App/AddAuthorCommand.php
+++ src/App/AddAuthorCommand.php
@@ -5,12 +5,13 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-const TEMPLATE_DIR = __DIR__ . DIRECTORY_SEPARATOR . 'Templates';
-const LIBRARY_DIR = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Library' . DIRECTORY_SEPARATOR . 'Authors';
-
-class CreateAuthorCommand extends Command
+class AddAuthorCommand extends Command
 {
+    private string $templatePath = __DIR__ . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR . 'Author.template';
+    private string $libraryPath = BASE_PATH . DIRECTORY_SEPARATOR . 'Library' . DIRECTORY_SEPARATOR . 'Authors';
+
     protected static $defaultName = 'add-author';
 
     protected function configure()
@@ -21,8 +22,12 @@ class CreateAuthorCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $helper = $this->getHelper('question');
+        $io = new SymfonyStyle($input, $output);
+
+        $io->title('Add a new author');
 
         // Ask for the type of author to add
+        $io->section('Author type');
         $question = new ChoiceQuestion(
             'What type of author would you like to add? (individual)',
             ['individual', 'organization'],
@@ -31,6 +36,7 @@ class CreateAuthorCommand extends Command
         $type = $helper->ask($input, $output, $question);
 
         // Ask for the display name of the author to add
+        $io->section('Author display name');
         $question = new Question('Please enter the display name of the author you are adding: ');
         $question->setValidator(function (?string $answer): string {
             if (empty($answer)) {
@@ -45,6 +51,8 @@ class CreateAuthorCommand extends Command
         $generatedID = preg_replace('/[^a-z0-9]/', '', strtolower($displayName));
 
         // Ask for an ID to use for the author
+        $io->section('Author ID');
+        $io->text('The ID can only contain lowercase letters and numbers.');
         $question = new Question("Please enter an ID you'd like to use for this author ($generatedID): ", $generatedID);
         $question->setValidator(function (?string $answer): string {
             if (class_exists('\\ExtensionDirectory\\Authors\\' . $answer)) {
@@ -60,6 +68,8 @@ class CreateAuthorCommand extends Command
         $id = $helper->ask($input, $output, $question);
 
         // Ask for a webpage URL for the author
+        $io->section('Author Webpage URL');
+        $io->text('The webpage URL must start with a protocol (HTTPS).');
         $question = new Question("Please enter a webpage URL for this author: ");
         $question->setValidator(function (?string $answer): string {
             if (!filter_var($answer, FILTER_VALIDATE_URL)) {
@@ -71,7 +81,7 @@ class CreateAuthorCommand extends Command
         $url = $helper->ask($input, $output, $question);
 
         // Generate the author file from the template
-        $template = file_get_contents(TEMPLATE_DIR . DIRECTORY_SEPARATOR . 'Author.template');
+        $template = file_get_contents($this->templatePath);
         $authorFileContent = strtr($template, [
             ':type:' => $type,
             ':name:' => $displayName,
@@ -80,7 +90,7 @@ class CreateAuthorCommand extends Command
         ]);
 
         // Write the author file to the library
-        if (!file_put_contents(LIBRARY_DIR . DIRECTORY_SEPARATOR . $id . '.php', $authorFileContent)) {
+        if (!file_put_contents($this->libraryPath . DIRECTORY_SEPARATOR . $id . '.php', $authorFileContent)) {
             return Command::FAILURE;
         }
 
